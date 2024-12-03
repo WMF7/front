43ff34a99bde5a68b33ff9f553baf022acab2826
Moataz Nabil <moataznabil2019@gmail.com>
2024-12-03 21:02:01 +0100

    add github actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Navigation } from './components/layout/Navigation';
 import { Banner } from './components/layout/Banner';
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Remove repository name from base URL
+  base: '/test-shop/', // Add repository name back to base URL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
